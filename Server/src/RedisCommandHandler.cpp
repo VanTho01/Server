8fb1e428c8ecc9bee4d3ad92d21bea8b5790bcaf
Nguyễn Văn Thơ <nguyenvanthohaui@gmail.com>
2025-11-10 22:37:32 +0700

    Set up command handler

---
+++ Server/src/RedisCommandHandler.cpp
@@ -0,0 +1,6 @@
+#include "../include/RedisCommandHandler.h"
+
+RedisCommandHandler::RedisCommandHandler()
+{
+
+}
--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include "../include/RedisServer.h"
+#include <thread>
+#include <chrono>
+
 int main(int argc, char* argv[])
 {
 	int port = 22;
 	if (argc >= 2) port = std::stoi(argv[1]);
 
-	RedisServer redisServer(port);
+	RedisServer server(port);
 	std::cout << "Server will listen on port: " << port << std::endl;
+	std::thread persistanceThread(
+		[]() {
+			while (true)
+			{
+				std::this_thread::sleep_for(std::chrono::seconds(300));
+			}
+		}
+	);
+
+	persistanceThread.detach();
+	server.run();
+
+	return 0;
+
 }
